13d0f23877ae1b15e9a74fb3138dc94ded176e92
lightless <lightless@foxmail.com>
2018-09-18 22:41:33 +0800

    [ADD] Update models. Add two keeper engines;

--- redstone/core/engines/keeper/task.py
+++ redstone/core/engines/keeper/task.py
@@ -12,6 +12,31 @@
     :copyright: Copyright (c) 2017 lightless. All rights reserved
 """
 
+import typing
 
-class TaskKeeper:
-    pass
+from ..base import SingleThreadBaseEngine, EngineStatus
+
+
+if typing.TYPE_CHECKING:
+    from redstone.core.application import RedstoneApplication
+
+
+class TaskKeeper(SingleThreadBaseEngine):
+    """
+    从TaskBufferQueue中获取任务放到ActiveMQ中
+    """
+    def __init__(self, app_ctx):
+        super(TaskKeeper, self).__init__()
+        self.name = "TaskKeeper"
+        self.app_ctx: RedstoneApplication = app_ctx
+
+    def start(self):
+        # TODO: Connect To ActiveMQ
+        super(TaskKeeper, self).start()
+
+    def _worker(self):
+
+        buffer_queue = self.app_ctx.BufferedQueues.REFRESH_TASK_BUFFER_QUEUE
+
+        while self._status == EngineStatus.RUNNING:
+            pass
